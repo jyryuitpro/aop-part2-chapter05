f9e4e6ebaf86edea802ea1e4fd5cf100fd16b247
jyr <jyr@wavers.co.kr>
2022-05-29 14:25:14 +0900

    03. 사진을 가져와서 목록 구성하기

--- app/src/main/java/io/jyryuitpro/aop/part2/chapter05/MainActivity.kt
+++ app/src/main/java/io/jyryuitpro/aop/part2/chapter05/MainActivity.kt
@@ -1,9 +1,15 @@
 package io.jyryuitpro.aop.part2.chapter05
 
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 
@@ -15,6 +21,19 @@ class MainActivity : AppCompatActivity() {
     private val startPhotoFrameModeButton: Button by lazy {
         findViewById<Button>(R.id.startPhotoFrameModeButton)
     }
+
+    private val imageViewList: List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(findViewById<ImageView>(R.id.imageView11))
+            add(findViewById<ImageView>(R.id.imageView12))
+            add(findViewById<ImageView>(R.id.imageView13))
+            add(findViewById<ImageView>(R.id.imageView21))
+            add(findViewById<ImageView>(R.id.imageView22))
+            add(findViewById<ImageView>(R.id.imageView23))
+        }
+    }
+
+    private val imageUriList: MutableList<Uri> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +51,7 @@ class MainActivity : AppCompatActivity() {
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
                 ) == PackageManager.PERMISSION_GRANTED -> {
                     // TODO 권한이 잘 부여되었을 때, 갤러리에서 사진을 선택하는 기능
+                    navigatePhotos()
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
                     // TODO 교육용 팝업 확인 후 권한 팝업을 띄우는 기능
@@ -40,6 +60,65 @@ class MainActivity : AppCompatActivity() {
                 else -> {
                     requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
                 }
+            }
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.d("MainActivity", "onRequestPermissionsResult Callback")
+        when (requestCode) {
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // TODO 권한이 부여된 것입니다.
+                    navigatePhotos()
+
+                } else {
+                    Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+
+            }
+        }
+    }
+
+    private fun navigatePhotos() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, 2000)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("MainActivity", "onActivityResult Callback")
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+
+        when (requestCode) {
+            2000 -> {
+                val selectedImageUri: Uri? = data?.data
+
+                if (selectedImageUri != null) {
+                    if (imageUriList.size >= 6) {
+                        Toast.makeText(this, "이미 사진이 꽉 찾습니다.", Toast.LENGTH_SHORT).show()
+                        return
+                    }
+
+                    imageUriList.add(selectedImageUri)
+                    imageViewList[imageUriList.size - 1].setImageURI(selectedImageUri)
+                } else {
+                    Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_SHORT).show()
             }
         }
     }
